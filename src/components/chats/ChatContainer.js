8be11577a0035d0aece2ad4b806fdfbfb68314af
Steven Winter <stevenwinter.me@gmail.com>
2019-04-14 20:58:14 -0500

    Fixed depricated icon imports

--- src/components/chats/ChatContainer.js
+++ src/components/chats/ChatContainer.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import SideBar from './SideBar'
 
-export class ChatContainer extends Component {
+export default class ChatContainer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            chats:[],
+            activeChat:null
+        }
+    }
+
+    setActiveChat = (activeChat)=>{
+        this.setState({activeChat})
+    }
+
     render() {
+        const { user, logout } = this.props
+        const { chats, activeChat } = this.state
         return (
-            <div></div>
+            <div className="container">
+                <SideBar
+                    logout={logout}
+                    chats={chats}
+                    user={user}
+                    activeChat={activeChat}
+                    setActiveChat={this.setActiveChat}
+                    />
+            </div>
         );
     }
 }
