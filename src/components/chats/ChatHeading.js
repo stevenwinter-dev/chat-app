bd7080f9cd2d12415e7d1a28ea0dfadaa02fbc48
Steven Winter <stevenwinter.me@gmail.com>
2019-04-17 21:45:06 -0500

    Completed ChatHeading and added styles to icons

--- src/components/chats/ChatHeading.js
+++ src/components/chats/ChatHeading.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { FaVideo } from 'react-icons/fa';
+import { FaUserPlus } from 'react-icons/fa';
+import { MdMoreHoriz } from 'react-icons/md';
 
-export default class ChatHeading extends Component {
-    render() {
-        return (
-            <div>Chat Heading</div>
-        );
-    }
+
+export default function({name, numberOfUsers}) {
+    const iconStyle = {
+        cursor: 'pointer',
+        fontSize: '2em',
+        marginRight: '.75em'
+    };
+
+    return (
+        <div className="chat-header">
+            <div className="user-info">
+                <div className="user-name">{name}</div>
+                <div className="status">
+                    <div className="indicator"></div>
+                    <span>{numberOfUsers ? numberOfUsers : null}</span>
+                </div>
+            </div>
+            <div className="option">
+                <FaVideo style={iconStyle} />
+                <FaUserPlus style={iconStyle} />
+                <MdMoreHoriz style={iconStyle} />
+            </div>
+        </div>
+    );
 }
